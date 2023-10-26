f7b83e019faae5317894e1af7cdbde162d102af1
mazmanyanashkhen911@gmail.com <mazmanyanashkhen911@gmail.com>
2023-10-26 04:33:37 +0400

    mi ban

--- FS-5/main.cpp
+++ FS-5/main.cpp
@@ -15,25 +15,31 @@ int main(int argc, char** argv) {
 
     char* path = argv[1];
 
-    int fd1 = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    int fd2 = dup(fd1);
-    if (fd1<0 || fd2<0)  {
+    int fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd)  {
         std::cerr << strerror(errno)<<std::endl;
         exit(errno);
     }
 
     int fd1 = dup(fd);
+    if (fd1)  {
+        std::cerr << strerror(errno)<<std::endl;
+        exit(errno);
+    }
     int fd2 = dup(fd);
-
+    if (fd2)  {
+        std::cerr << strerror(errno)<<std::endl;
+        exit(errno);
+    }
     
     std::string l1 = "first line\n"; 
     std::string l2 = "second line\n";
 
-    write(fd1, l1.c_str(),l1.lenght());
-    write(fd2, l2.c_str(),l2.lenght());
+    int written_bytes1 = write(fd1, l1.c_str(),l1.lenght());
+    int written_bytes2 =  write(fd2, l2.c_str(),l2.lenght());
 
     close(fd1);
     close(fd2);
-
+    close(fd);
     return 0;
 }
